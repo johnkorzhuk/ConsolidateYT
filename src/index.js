6f1b3cd8761e8848ce03f752078e9f6bc8623420
John Korzhuk <jkorzhuk@gmail.com>
2017-03-13 10:34:12 -0400

    changed landing page layout

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import 'normalize.css'
+import './css/grid.css'
 import './css/layout.css'
 import './css/main.css'
-
 
 import './js/auth'
 import './js/start'
--- src/js/start.js
+++ src/js/start.js
@@ -2,19 +2,17 @@ import $ from 'jquery'
 
 import { updateNextSpotifyQueryList } from './store'
 import { getSpotifyData, getDummyData } from './api/spotify'
-import renderSearch from './ui/SearchBar/searchBar'
 import renderTable from './ui/SongResultTable/songResultTable'
 
 $(() => {
-  $('.js-start').click(() => {
-    renderSearch()
-  })
+  // $('.js-start').click(() => {
+  //   renderSearch()
+  // })
 
   $('.js-search-form').submit((e) => {
-    const query = e.target[0].value
     e.preventDefault()
 
-    // getSpotifyData(query)
+    // getSpotifyData(e.target[0].value)
     //   .then((data) => {
     //     updateNextSpotifyQueryList(data.tracks.next)
     //     $('.song-data').html(renderTable($('.song-data'), data.tracks.items))
@@ -26,6 +24,29 @@ $(() => {
     //   .catch(() => {
     //     $('.song-data').html('<h2>Oh no! Something went wrong :(</h2>')
     //   })
+    $('.js-intro')
+      // .removeClass('container')
+      .css({
+        marginTop: 0,
+        alignSelf: 'flex-start'
+      })
+      .children()
+      .css('font-size', '2.5rem')
+      .not('h1')
+      .remove()
+
+    $('.js-hero').css({
+      justifyContent: 'flex-start'
+    })
+
+    $('.js-form-container').css({
+      display: 'block',
+      marginTop: '30vh'
+    })
+    $('.search-form').css('margin-bottom', '3rem')
+    $('#js-search').focus()
+
+    $('.js-start').off()
     getDummyData()
       .then((data) => {
         $('.song-data').html(renderTable($('.song-data'), data))
